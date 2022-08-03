0c3896ea053f84c82adda794f396a67158ce6d3c
nonyil <igbrendow@gmail.com>
2022-08-03 18:31:02 -0300

    feat: :fire: New configs
    userRespository, createUserDTO, createUseCase

--- src/useCases/CreateUser/CreateUserUserCase.ts
+++ src/useCases/CreateUser/CreateUserUserCase.ts
@@ -1,5 +1,20 @@
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
 export class CreateUserUseCase {
-  async execute() {
-    
+  constructor(
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlredyExists = await this.usersRepository.findByEmail(data.email);
+
+    if (userAlredyExists) {
+      throw new Error("User already exists.");
+    }
+
+    const user = new User(data);
+
+    await this.usersRepository.save(user);
   }
 }
